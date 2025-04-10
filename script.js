519e388f8912e02c0a3e97c019ccdae0f0ee0219
alirezamohammadpoor <alirezamohammadp@gmail.com>
2025-04-10 15:33:16 +0200

    Changed Book Constructor to a Class

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const bookContainer = document.querySelector(".container");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
+class Book {
+  constructor(title, author, pages, read) {
+    if (!new.target) {
+      throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.bookId = crypto.randomUUID();
   }
 
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.bookId = crypto.randomUUID();
-  this.info = function () {
+  info() {
     let readStatus;
     if (this.read === true || this.read === "read") {
       readStatus = "read";
@@ -28,7 +31,7 @@ function Book(title, author, pages, read) {
       " pages, " +
       readStatus
     );
-  };
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
